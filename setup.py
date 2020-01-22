cdcee8d3e8656c38624be7f2f45c0a74867eed56
michelledu008 <37251297+michelledu008@users.noreply.github.com>
2020-01-22 11:37:33 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='unet',
+    version='0.1',
+    description='Sample Unet',
+    url='https://github.com/michelledu008/unet.git',
+    packages=[
+        'unet'
+        ])
